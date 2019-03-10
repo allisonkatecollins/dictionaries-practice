fa30e3f49549f8edf85b1d34144c97ea46054497
Allison Collins <allisonkatecollins@gmail.com>
2019-03-10 18:23:11 -0500

    finished planets exercise

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace dictionaries_practice
   {
     static void Main(string[] args)
     {
+      //STOCK PURCHASE DICTIONARIES
       Dictionary<string, string> stocks = new Dictionary<string, string>();
       stocks.Add("GM", "General Motors");
       stocks.Add("CAT", "Caterpillar");
@@ -65,10 +66,12 @@ namespace dictionaries_practice
               stockReport.Add(stocks[kvp.Key], kvp.Value);
             }
           }
-            foreach (KeyValuePair<string, double> item in stockReport) {
+            /* foreach (KeyValuePair<string, double> item in stockReport) {
               Console.WriteLine($"The position in {item.Key} is worth {item.Value}");
-            }
-          /* 
+            } */
+
+          /* ----OUTPUT----
+
             The position in General Motors is worth 230.21
             The position in General Motors is worth 811.19
             The position in General Motors is worth 1217.53
@@ -84,6 +87,45 @@ namespace dictionaries_practice
             The position in Dow Jones is worth 1209.88
           */
         }
+      }
+    //ITERATING OVER PLANETS
+
+    //1. Create new list with all 8 planets
+    List<string> planetList = new List<string>(){"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
+    
+    //2. Create another list containing dictionaries.
+    //Each dictionary will hold the name of a spacecraft that we have launched, and the name of a planet it has visited.
+    List<Dictionary<string, string>> probes = new List<Dictionary<string, string>>();
+      probes.Add(new Dictionary<string, string>(){{"Mercury", "Mariner 10"}});
+      probes.Add(new Dictionary<string, string>(){{"Venus", "Galileo"}});
+      probes.Add(new Dictionary<string, string>(){{"Earth", "International Space Station"}});
+      probes.Add(new Dictionary<string, string>(){{"Mars", "Viking 1 Orbiter"}});
+      probes.Add(new Dictionary<string, string>(){{"Jupiter", "Voyager 1"}});
+      probes.Add(new Dictionary<string, string>(){{"Saturn", "Pioneer 11"}});
+      probes.Add(new Dictionary<string, string>(){{"Uranus", "Voyager 2"}});
+      probes.Add(new Dictionary<string, string>(){{"Neptune", "Voyager 2"}});
+   
+    //3. Iterate over planetList, and inside that loop, iterate over the list of dictionaries
+    //For each planet, write to the console which satellites have visited which planet.
+    foreach (string planet in planetList) //iterate planets
+      {
+        List<string> matchingProbes = new List<string>();
+
+        foreach(Dictionary<string, string> probe in probes) //iterate probes
+        {
+          //Does the current Dictionary contain the key of the current planet?
+          //If so, add the current spacecraft to 'matchingProbes'.
+          if (probe.ContainsKey(planet)) {
+            foreach (KeyValuePair<string, string> kvp in probe) {
+            matchingProbes.Add(kvp.Value);
+          }
+        }
+      }
+        //Use String.Join(",", matchingProbes) as part of the solution to get output below
+        //It's the C# way of writing 'array.join(",") in JavaScript.
+        //Console.WriteLine($"{}: {}");
+        
+        Console.WriteLine($"{planet}: {String.Join(",", matchingProbes)}");
       }
     }
   }
